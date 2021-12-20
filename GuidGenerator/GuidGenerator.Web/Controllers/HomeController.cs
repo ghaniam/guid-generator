e417e58af4e8792d348c9e2163fea16366cc68d3
g.malik <g.malik@brainbay.nl>
2021-12-20 19:00:44 +0100

    first commit

---
+++ GuidGenerator/GuidGenerator.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using GuidGenerator.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuidGenerator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string GuidsViewDataKey = "GuidsViewData";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[GuidsViewDataKey] = new List<Guid>() { Guid.NewGuid() };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int guidAmount)
+        {
+            var guids = new List<Guid>();
+            for(var i = 0; i < guidAmount; i++)
+                guids.Add(Guid.NewGuid());
+
+            ViewData[GuidsViewDataKey] = guids;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
